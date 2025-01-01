dcc0149ffffd0b015b1ce7645e42f3406a162ceb
Michael Forde <michael.kenneth.forde@gmail.com>
2025-01-01 12:18:41 -0500

    Solve reverseString exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let arr = word.split(" ");
+  let result = "";
+
+  for (let i = arr.length; i > 0; i--) {
+    let reversedWord = "";
+    for (let j = arr[i-1].length - 1; j >= 0; j--) {
+      reversedWord += arr[i-1].at(j);
+    }
+    result += reversedWord;
+
+    if (i != 1) {
+      result += " ";
+    }
+  }
+
+  return result;
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
