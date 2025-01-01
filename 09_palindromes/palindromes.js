3778f94d01ca7dbe93375089ca983a4529b6323e
Michael Forde <michael.kenneth.forde@gmail.com>
2025-01-01 13:56:37 -0500

    Solve palindromes exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (sentenceRaw) {
+  const sentenceNoSpecialChar = sentenceRaw.replace(/[.,!]/g, "").toLowerCase();
+  let sentenceArray = sentenceNoSpecialChar.split(", "); // array
+
+  let array = [];
+  let palindrome = true;
+  for (let word of sentenceArray) { 
+      let opposite = "";
+      for (let i = word.length -1; i >= 0; i--) {
+        opposite += word.at(i)
+      }
+      array.push(opposite);
+  }
+  // back to string
+  let reversedSentence = array.toString(); 
+  let sentence = sentenceArray.toString();
+  sentence = sentence.replace(/[" "]/g, "");
+  reversedSentence = reversedSentence.replace(/[" "]/g, "");
+
+  if (sentence != reversedSentence) { palindrome = false;}
+
+  return palindrome;
 
 };
 
