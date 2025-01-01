f607d8328b5115a24d377fa3601c93761b3760af
Michael Forde <michael.kenneth.forde@gmail.com>
2025-01-01 12:59:55 -0500

    Solve sumAll exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
+
 
 };
 
