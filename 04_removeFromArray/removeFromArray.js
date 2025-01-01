ec8ed4ef11e9d99cdd4247ca1e1eacd3d55335ea
Michael Forde <michael.kenneth.forde@gmail.com>
2025-01-01 12:50:54 -0500

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  // let array = array.slice(0);
+
+  for (let arg of args) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === arg) {
+        array.splice(i,1);
+        i--;
+      }
+    }
+  }
+  return array; 
 };
 
 // Do not edit below this line
